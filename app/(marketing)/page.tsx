2605261b1e3de5d9652e61237902c88a05f09fc4
ispasdani <ispasdani14@gmail.com>
2026-04-10 15:01:59 +0200

    convex integration done

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,15 +1,21 @@
+'use client'
+
 import Hero from "@/components/marketing-sections/hero";
 import Features from "@/components/marketing-sections/features";
 import GameEngine from "@/components/marketing-sections/game-engine";
 import Pricing from "@/components/marketing-sections/pricing";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 export default function Home() {
+    const tasks = useQuery(api.tasks.get);
     return (
         <main>
             <Hero />
             <Features />
             <GameEngine />
             <Pricing />
+            {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
         </main>
     );
 }
